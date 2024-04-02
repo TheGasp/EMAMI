0c2a8336b06b7759ffd055826015f79ce99ba5f5
vanes <vanessa.chambon@etu.mines-ales.fr>
2024-04-02 17:56:04 +0200

    interface graphique, premier page, s'enregistrer et connexion

--- app/src/main/java/com/flamboox/emami_new_version/MainActivity.java
+++ app/src/main/java/com/flamboox/emami_new_version/MainActivity.java
@@ -1,14 +1,53 @@
 package com.flamboox.emami_new_version;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextEmail, editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewForgotPassword,textViewSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        textViewSignUp = findViewById(R.id.textViewSignUp);
+
+
+
+
+        this.textViewForgotPassword.setOnClickListener(v -> {
+            Intent ouvrirChangerMdP = new Intent(MainActivity.this, MainActivityMDP.class);
+            startActivity(ouvrirChangerMdP);
+        });
+
+
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ouvrirInscrip1 = new Intent(MainActivity.this, MainActivityCreation.class);
+                startActivity(ouvrirInscrip1);
+            }
+        });
+
+
+
+
     }
+
 }
+
+
---
+++ app/src/main/java/com/flamboox/emami_new_version/MainActivityMDP.java
@@ -0,0 +1,61 @@
+package com.flamboox.emami_new_version;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import java.util.Random;
+import java.util.Properties;
+
+public class MainActivityMDP extends AppCompatActivity {
+    private EditText editTextEmailAddress;
+    Button button;
+    EditText sendto, subject, body;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_mdp);
+        sendto = findViewById(R.id.editTextEmail);
+        String subject = "Réinitialisation du mot de passe ";
+        String body = "votre nouveau mot de passe est:" + generateNewPassword();
+        button = findViewById(R.id.buttonLogin);
+
+        button.setOnClickListener(view -> {
+            String emailsend = sendto.getText().toString();
+            String emailsubject = subject.toString();
+            String emailbody = body.toString();
+
+            Intent intent = new Intent(Intent.ACTION_SEND);
+
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{emailsend});
+            intent.putExtra(Intent.EXTRA_SUBJECT, emailsubject);
+            intent.putExtra(Intent.EXTRA_TEXT, emailbody);
+
+            intent.setType("message/rfc822");
+
+            startActivity(Intent.createChooser(intent, "Choose an Email client :"));
+        });
+
+    }
+    private String generateNewPassword() {
+        Random random = new Random();
+
+        char[] CHARACTERS = {'@', '/', ':', '?', '!', ';', '#', '$'};
+        int index = random.nextInt(CHARACTERS.length);
+        char a = CHARACTERS[index];
+
+        StringBuilder lettreMdP = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            boolean minuscule = random.nextBoolean(); //choisit 0 ou 1 aléatoirement pour avoir une majuscule ou une min
+            char randomChar = (char) (random.nextInt(26) + (minuscule ? 'a' : 'A'));
+            lettreMdP.append(randomChar);
+        }
+        String b = lettreMdP.toString();
+
+        int c = random.nextInt(90) + 10;
+
+        return Character.toString(a) + b + c;}}
