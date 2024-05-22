9183656cc787639971b6718bc10764f25fd3ce1e
vanes <vanessa.chambon@etu.mines-ales.fr>
2024-05-22 16:18:52 +0200

    interface graphique, premier page, s'enregistrer et connexion

--- app/src/main/java/com/flamboox/emami_new_version/MainActivityReseau.java
+++ app/src/main/java/com/flamboox/emami_new_version/MainActivityReseau.java
@@ -3,12 +3,26 @@ package com.flamboox.emami_new_version;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivityReseau extends AppCompatActivity {
+    Button BoutonInscrit;
+    EditText Facebook, Instagram, Twitter, Snapchat, Discord, LinkedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_reseau);
+
+        BoutonInscrit = findViewById(R.id.buttonInscrit);
+
+        Facebook = findViewById(R.id.editTextFacebook);
+        Instagram = findViewById(R.id.editTextInstagram);
+        Twitter = findViewById(R.id.editTextTwitter);
+        Snapchat = findViewById(R.id.editTextSnap);
+        Discord = findViewById(R.id.editTextDiscord);
     }
+
 }
